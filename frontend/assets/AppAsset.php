98c07e0cc3d15a53bb4f523a409f09ed409629bd
ansuns <251024883@qq.com>
2018-08-31 17:06:35 +0800

    布局

--- frontend/assets/AppAsset.php
+++ frontend/assets/AppAsset.php
@@ -9,13 +9,25 @@ use yii\web\AssetBundle;
  */
 class AppAsset extends AssetBundle
 {
-    public $basePath = '@webroot';
-    public $baseUrl = '@web';
+    public $basePath = '@webroot/sourcePath';
+    public $baseUrl = '@web/static';
     public $css = [
-        'css/site.css',
+        "css/bootstrap.min.css",
+        "css/font-awesome.min.css",
+        "css/animate.min.css",
+        "css/lightbox.css",
+        "css/main.css",
+        "css/responsive.css",
     ];
     public $js = [
+        "js/jquery.js",
+        "js/bootstrap.min.js",
+        "js/lightbox.min.js",
+        "js/wow.min.js",
+        "js/main.js",
+
     ];
+    //public $jsOptions = ['condition' => 'lt IE9','js'=>['js/html5shiv.js','js/respond.min.js']];
     public $depends = [
         'yii\web\YiiAsset',
         'yii\bootstrap\BootstrapAsset',
