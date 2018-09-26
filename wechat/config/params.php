eec039aedbb62ec9d7aa15404b5562640533c277
ansuns <251024883@qq.com>
2018-09-26 14:48:21 +0800

    easywechat

--- wechat/config/params.php
+++ wechat/config/params.php
@@ -4,7 +4,7 @@ return [
     'wechat' => [
         'app_id' => 'wxdc731455d1970f9e',
         'secret' => 'b3e55f3562dd316e0a2eb0a2eebbcedc',
-
+        'token' => 'wechat',
         // 指定 API 调用返回结果的类型：array(default)/collection/object/raw/自定义类名
         'response_type' => 'array',
 
