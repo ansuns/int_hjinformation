73a074a872317f5f0aee045b7d490296617344af
ansuns <251024883@qq.com>
2018-09-26 17:28:03 +0800

    easywechat

--- wechat/controllers/DefaultController.php
+++ wechat/controllers/DefaultController.php
@@ -27,7 +27,37 @@ class DefaultController extends Controller
         $app = Factory::officialAccount($config);
 
         $app->server->push(function ($message) {
-            return "您好！";
+            switch ($message['MsgType']) {
+                case 'event':
+                    return '收到事件消息';
+                    break;
+                case 'text':
+                    return '收到文字消息';
+                    break;
+                case 'image':
+                    return '收到图片消息';
+                    break;
+                case 'voice':
+                    return '收到语音消息';
+                    break;
+                case 'video':
+                    return '收到视频消息';
+                    break;
+                case 'location':
+                    return '收到坐标消息';
+                    break;
+                case 'link':
+                    return '收到链接消息';
+                    break;
+                case 'file':
+                    return '收到文件消息';
+                // ... 其它消息
+                default:
+                    return '收到其它消息';
+                    break;
+            }
+
+            // ...
         });
 
         $response = $app->server->serve();
