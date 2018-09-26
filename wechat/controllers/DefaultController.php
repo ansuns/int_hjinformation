e55eb11f5a3047a0978098c95befbaccff7aa9ac
ansuns <251024883@qq.com>
2018-09-26 15:15:04 +0800

    easywechat

--- wechat/controllers/DefaultController.php
+++ wechat/controllers/DefaultController.php
@@ -24,11 +24,13 @@ class DefaultController extends Controller
     {
         $config = Yii::$app->params['wechat'];
         $app = Factory::officialAccount($config);
-        $response = $app->server->serve();
 
         $app->server->push(function ($message) {
             return "您好！:".$message;
         });
+
+        $response = $app->server->serve();
+
         // 将响应输出
         $response->send(); // Laravel 里请使用：return $response;
 
