493ae04ad0ba3cdde73005b8f15e4fe423aeec78
ansuns <251024883@qq.com>
2018-09-26 14:52:17 +0800

    easywechat

--- wechat/config/params.php
+++ wechat/config/params.php
@@ -10,7 +10,7 @@ return [
 
         'log' => [
             'level' => 'debug',
-            'file' => __DIR__.'/wechat.log',
+            'file' => Yii::$app->getRuntimePath() . "/logs/wechat.log',
         ],
     ]
 ];
